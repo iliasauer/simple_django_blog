fe5a49f6222843f4f54f83f521cecc4c9bd77434
iliasauer <ilia_kisliakovskii@hotmail.com>
2015-12-20 15:13:11 +0300

    First commit

---
+++ article/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Article(models.Model):
+    class Meta:
+        db_table = "article"
+    article_title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    article_date = models.DateTimeField()
+    article_likes = models.IntegerField()
